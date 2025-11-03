a4c150fdedef0e749b350e2827380e634dcd35b8
212e1 <igorkirciu2007@gmail.com>
2025-11-03 23:40:27 +0200

    1

---
+++ kirciu/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace kirciu.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SiteInformation()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ kirciu/wwwroot/js/shared_script.js
@@ -0,0 +1,9 @@
+﻿// Изменение цвета шапки при прокрутке
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header-top');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
